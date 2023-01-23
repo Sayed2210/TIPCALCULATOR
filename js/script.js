5feeae60925ccaad8a5ed2901c38bd3647a6124c
Elsayed Hassan <eh600039@gmail.com>
2023-01-23 17:22:18 +0200

    finish UI

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//craete calc from buuton
+let calc = document.getElementById('claculator');
+calc.addEventListener("click", ()=> {
+    let billamount =document.getElementById('bill').value;
+    let 
+})
