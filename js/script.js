02d59d28d1d7449cad0e8cdee93c594e3651c31a
Elsayed Hassan <eh600039@gmail.com>
2023-01-23 18:07:22 +0200

    add function to app

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-//craete calc from buuton
+//add fun and action for buuton
 let calc = document.getElementById('claculator');
 calc.addEventListener("click", ()=> {
     let billamount =document.getElementById('bill').value;
-    let 
+    let serviceAmount =document.getElementById('tips').value;
+    let numOfPeople = document.getElementById('people').value;
+    if (billamount === '' || serviceAmount == 0 ) {
+        alert('please Enter amount and bill')
+    }
+    if (numOfPeople === '' || numOfPeople <= 1) {
+        numOfPeople = 1;
+    }
+    else {
+        document.getElementById('each').style.display= "block";
+    }
+    //calc tip
+    let total =((billamount * serviceAmount) / numOfPeople).toFixed(2);
+    document.querySelector('.total').style.display= "block";
+    document.getElementById('amount').innerHTML= total;
 })
